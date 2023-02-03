b066d911dd42acacdae999c9a9df822ddac3dbfd
mohanrajmurus <mohanrajmurus@gmail.com>
2023-02-03 14:46:23 +0530

    media query update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from '../images/logo.svg';
+import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai'
 const NavBar = () => {
+  const [isopen, setOpen] = useState(true);
   const menuItems = ['Home','LandLord','Tenants','Contact'];
   return (
     <nav className='nav'>
           <div className='nav__logo'>
             <img src={logo} alt='home-agency' />
           </div>
-          <ul className='nav--menu'>
+          <ul className={isopen?'nav--menu':'mobile__menu'}>
             {menuItems.map((item,i) => {
               return (
                 <li className='nav--menu__items' key={i}>
@@ -34,6 +36,10 @@ const NavBar = () => {
               className={({isActive})=>isActive?'active':undefined}>Contact Us</NavLink>
             </li> */}
           </ul>
+          <span className='menubar' 
+          onClick={()=> setOpen(!isopen)}>
+            {isopen?<AiOutlineMenu/>:<AiOutlineClose />}
+          </span>
         </nav>
   )
 }
